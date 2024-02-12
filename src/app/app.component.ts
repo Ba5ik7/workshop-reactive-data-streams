4a4cab226a5055bf187da25e7cc5e05ff7a565d3
Wesley DuSell <dusellfam@gmail.com>
2024-02-11 23:38:53 -0500

    AsyncPipe, of, from

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: `
-  <h1>{{ title }}</h1>
+  <h1>{{ title$ | async}}</h1>
+  <h1>{{ titles$ | async}}</h1>
     <router-outlet></router-outlet>
   `,
 })
 export class AppComponent {
   title = 'Idiomatic Reactive Data Streams';
+  title$ = of(this.title);
+  titles$ = from(this.title);
 }
